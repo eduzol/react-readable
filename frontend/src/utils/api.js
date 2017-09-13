fd614e7bdbe75c152240eaa19231215dd68c24d0
EduardoZola <eduzol@gmail.com>
2017-09-13 01:02:51 -0700

    small refactor to api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,22 +1,22 @@
 const api =  process.env.REACT_READABLE_API || 'http://localhost:3001';
 
 let token = localStorage.token;
-console.log('token ' , token);
 if (!token) {
     localStorage.token = Math.random().toString(36).substr(-8);
     token = localStorage.token;
 }
 
+console.log('token ' , token);
 const headers = {
     'Accept': 'application/json',
     'Authorization': token
   }
 
-export const getCategories = () =>
-    fetch(`${api}/categories`, {headers})
+export const getCategories = () =>{
+    return fetch(`${api}/categories`, {headers})
         .then(res =>  res.json())
         .then( data => data.categories )
-
+}
 export const getPosts = (category) => {
     category = category.trim();
     return fetch(`${api}/${category}/posts`,  {headers})

ec515a6e9fee865607d16c01ac2e581f153471ba
EduardoZola <eduzol@gmail.com>
2017-09-23 23:45:00 -0700

    comment out code

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -110,3 +110,60 @@ export const deleteComment = (id) => {
     .then(res => res.json());
 }
  
+/*
+    ReadableAPI.getCategories().then((categories) => {
+      console.log('categories ', categories );
+    });
+
+    ReadableAPI.getPostsByCategory('react').then((posts) => {
+      console.log('react posts ' ,posts );
+    });
+
+    ReadableAPI.getPosts().then((posts) => {
+      console.log(' react posts ' ,posts );
+    });
+
+    ReadableAPI.getPostById('8xf0y6ziyjabvozdd253nd').then((post) => {
+      console.log('post ' , post);
+    });
+
+    ReadableAPI.getPostComments('8xf0y6ziyjabvozdd253nd').then((comments) => {
+      console.log('comments ' , comments);
+    });
+
+    var post = {
+      id : Math.random().toString(36).substr(-8) , 
+      timestamp: Date.now(),
+      title : 'Sample Eduardos Post', 
+      body : 'Udacity is so cool', 
+      author : 'eduzol', 
+      category : 'react'
+    };
+
+    ReadableAPI.addPost(post).then((response) => {
+      console.log('response ' , response );
+    });
+
+    ReadableAPI.votePost('8xf0y6ziyjabvozdd253nd', 'upVote').then((response) => {
+      console.log('vote' , response );
+    });
+
+    ReadableAPI.editPost('8xf0y6ziyjabvozdd253nd', {title:'Hello' , body: 'world'}).then((response) => {
+      console.log('edit post ' , response );
+    });
+
+    ReadableAPI.deletePost('jsnga9oh').then((response) => {
+      console.log('delete post ' , response );
+    });
+   
+    var newComment = {
+      id: Math.random().toString(36).substr(-8),
+      timestamp: Date.now(),
+      body:'Es Miranda mi amor',  
+      owner: 'eduzol',  
+      parentId: 'oqnpmmqp'
+    };
+    ReadableAPI.commentPost(newComment).then((response) => {
+      console.log('new post **** ' , response );
+    });
+ */

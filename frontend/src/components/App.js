ac709f5aea65bfc2b11c9b55f499590bc6078815
EduardoZola <eduzol@gmail.com>
2017-09-24 01:30:04 -0700

    Categories component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import * as  ReadableAPI from '../utils/api.js'; 
+import CategoriesList from './CategoriesList';  
 
 class App extends Component {
+
+  state = {
+    categories :[]
+  };
+
+  componentDidMount(){
+    ReadableAPI.getCategories().then((categories) => {
+      console.log('categories ', categories );
+      this.setState({categories});
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <CategoriesList categories={this.state.categories} />
       </div>
     );
   }

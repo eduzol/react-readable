b27ef3cc25b2727f22f5c434f59d54f5d56b1a34
EduardoZola <eduzol@gmail.com>
2017-10-30 23:07:58 -0700

    Add ability to vote posts on post list view

--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,23 +1,66 @@
 import React, { Component } from 'react';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import { Glyphicon} from 'react-bootstrap';
 import * as moment from 'moment/moment';
 import { connect } from 'react-redux';
+import {  loadPost } from '../actions';
+import * as  ReadableAPI from '../utils/api.js'; 
 import { withRouter } from 'react-router-dom';
 import {Link} from 'react-router-dom';
 
 class PostList extends Component{
 
-    dateFormatter (cell, row){
+    dateFormatter = (cell, row) => {
         let formattedDate = moment.unix(cell/1000).format("MM-DD-YYYY HH:mm");
         return formattedDate;
     }
 
-    linkFormatter (cell, row) {
+    upVote  = (element) => {
+        element.preventDefault();
+        let postId =  element.currentTarget.getAttribute('name');
+        
+        ReadableAPI.votePost(postId, 'upVote').then((response) => {
+            this.props.loadPost(response);
+        });
+        
+    }
+
+    downVote = (element) => {
+        element.preventDefault();
+        let postId =  element.currentTarget.getAttribute('name');
+        
+        ReadableAPI.votePost(postId, 'downVote').then((response) => {
+            this.props.loadPost(response);
+        });
+      
+    }
+
+    scoreFormatter = (cell, row) => {
+        
+        let postId = row.id ;
+        return (
+            <span>
+
+                {cell} &nbsp;  &nbsp;
+                <a  name={postId} role="button" onClick={this.upVote}><Glyphicon glyph="arrow-up" /></a>
+                &nbsp;
+                <a  name={postId} role="button" onClick={this.downVote}><Glyphicon glyph="arrow-down" /></a>
+               
+            </span>
+        );        
+       
+    }
+
+    linkFormatter  = (cell, row) =>  {
+        
+        let postId = row.id ;
         
         return (
-            <Link to={'/post/'+row.id}>
+            <span>
+            <Link to={'/post/'+postId}>
                 {cell}
              </Link>
+            </span>
         );
     }
 
@@ -39,9 +82,10 @@ class PostList extends Component{
         return (
             <div className="list-component posts">
                 <BootstrapTable data={posts} hover keyField='id' trClassName='tr-background'>
-                  <TableHeaderColumn dataField='title' width='70%' dataFormat={this.linkFormatter}>Posts</TableHeaderColumn>
+                  <TableHeaderColumn dataField='title' width='58%' dataFormat={this.linkFormatter}>Posts</TableHeaderColumn>
+                  <TableHeaderColumn dataField='author' width='10%' dataSort={ true } >Author</TableHeaderColumn>
                   <TableHeaderColumn dataField='timestamp' width="20%" dataSort={ true } dataFormat={this.dateFormatter}>Date</TableHeaderColumn>
-                  <TableHeaderColumn dataField='voteScore' width="10%" dataSort={ true }>Score</TableHeaderColumn>
+                  <TableHeaderColumn dataField='voteScore' width="12%" dataSort={ true } dataFormat={this.scoreFormatter} >Score</TableHeaderColumn>
                 </BootstrapTable>
             </div>
         );
@@ -55,4 +99,11 @@ function  mapStateToProps (state ){
     };
 }
 
-export default withRouter(connect(mapStateToProps)(PostList));
+
+function mapDispatchToProps(dispatch){
+    return {
+      loadPost : (data) => dispatch(loadPost(data))
+    }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(PostList));
